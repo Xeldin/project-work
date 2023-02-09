f23800d59f64750bf2c9d2791eb0d538af1cdd0d
Daniele Ghilardini <ghilardinidaniele@gmail.com>
2023-02-09 15:44:00 +0100

    Updated news

--- cms/src/app/news/news.component.ts
+++ cms/src/app/news/news.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { news } from './news.module';
+import { NewsService } from './service/news.service';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.css']
+  styleUrls: ['./news.component.css'],
 })
-export class NewsComponent {
+export class NewsComponent implements OnInit {
+  dati$!: Observable<news[]>;
+  newNews = '';
 
+  constructor(private newsService: NewsService) {}
+
+  ngOnInit(): void {
+    this.dati$ = this.newsService.getNews();
+  }
+
+  onAddNews() {
+    const newNews = {
+      news: this.newNews,
+      completed: false,
+    };
+    this.newsService.AddNews(newNews).subscribe(() => {
+      this.dati$ = this.newsService.getNews();
+    });
+  }
+  onClickElimina(id: string) {
+    this.newsService.deleteNews(id).subscribe(() => {
+      this.dati$ = this.newsService.getNews();
+    });
+  }
 }
---
+++ cms/src/app/news/news.module.ts
@@ -0,0 +1,10 @@
+export interface news {
+  _id: string;
+  title: string;
+  category: string;
+  imageUrl: string;
+  content: string;
+  publicationDate: string;
+  authorName: string;
+  tags: string[];
+}
