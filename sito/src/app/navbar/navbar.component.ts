4597c158f1fe9289349094faafd98905d632d442
Moneta02 <pietro.coin02@gmail.com>
2023-02-15 16:07:42 +0100

    added navbar e footer

--- sito/src/app/navbar/navbar.component.ts
+++ sito/src/app/navbar/navbar.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
-
+  onSearch(event: Event) {
+    event.preventDefault();
+    // TODO: implement search functionality
+  }
 }
