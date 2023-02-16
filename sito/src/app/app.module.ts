f284bc1ff94705900efc7d08df470e4b5b9aba75
Moneta02 <pietro.coin02@gmail.com>
2023-02-16 16:58:15 +0100

    add search function

--- sito/src/app/app.module.ts
+++ sito/src/app/app.module.ts
@@ -5,18 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { FilterPipeComponent } from './navbar/filter-pipe/filter-pipe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    FilterPipeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
